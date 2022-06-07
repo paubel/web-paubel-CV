39d0107194b0d7828ac1afa2b211e39d738aeecd
paubel <paul.belfrage@gmail.com>
2022-06-07 12:23:49 +0200

    upload 2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const navigationHeight = document.querySelector(
   ".primary-navigation"
 ).offsetHeight;
-
-console.log(document.documentElement.style);
+/* 
+console.log(document.documentElement.style); */
 
 document.documentElement.style.setProperty(
   "--scroll-padding",
   navigationHeight - 1 + "px"
 );
 
-console.log(document.documentElement.style);
+/* console.log(document.documentElement.style); */
